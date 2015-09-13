1db8581bbf7592775d52ecd0e7f06107a821c4bf
Tom Giordano <justom@gmail.com>
2015-09-13 13:39:39 -0400

    Cleanup

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 # This is a check of the auto-grader's fenceposting.
 # In theory "at least 3" should be >= 3, not > 3
 
-#Interactive version
+# Interactive version
 puts "What's your name"
 my_name = gets.strip
 
