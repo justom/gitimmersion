4bd5639ccf3f81fd17e6cee85ce1b953aef0130e
Tom Giordano <justom@gmail.com>
2015-09-13 13:21:17 -0400

    bad comment.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
 # This is a check of the auto-grader's fenceposting.
 # In theory "at least 3" should be >= 3, not > 3
 
+#BAD YAMS
+
 #Interactive version
 puts "What's your name"
 my_name = gets.strip
