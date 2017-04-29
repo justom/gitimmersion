733b095218f13fea9d7aef64ea004b972037e4c8
Tom Giordano <justom@gmail.com>
2017-04-28 23:31:58 -0400

    Just another comment.

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,6 +3,8 @@
 
 # This is a check of the auto-grader's fenceposting.
 # In theory "at least 3" should be >= 3, not > 3
+#
+# This is apparently not true, but that's okay.
 my_name = ARGV.first || "World"
 puts "Hello, #{my_name}!"
 
