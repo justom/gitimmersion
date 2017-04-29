386d870c7b6bdd33dd71fc0db8784d858b2cbda3
Tom Giordano <justom@gmail.com>
2017-04-28 23:25:08 -0400

    Interim commit

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,12 +3,14 @@
 
 # This is a check of the auto-grader's fenceposting.
 # In theory "at least 3" should be >= 3, not > 3
+my_name = ARGV.first || "World"
+puts "Hello, #{my_name}!"
 
 # Interactive version
 puts "What's your name?"
-my_name = gets.strip || "World"
+new_name = gets.strip
 
 # And one more commit for good measure
-puts "Hello, #{my_name}!"
+puts "Hello, #{new_name}!"
 puts "I wonder if this is testing correctly."
 
